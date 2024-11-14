567864dc571eaee63441d18babb83c4258f15bd7
Vinibrasil01 <viniciosbrasil01@gmail.com>
2024-11-14 23:12:31 +0100

    creacion de publicar

---
+++ Pokemoncard_shop/app/view/publicar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="publicar.css">
+</head>
+<body>
+<header>
+        <img class="img-logo" src="imagenes/image.png" alt="logo">
+        <nav>
+            <ul>
+
+                <li><a href="inicio.html">Inicio</a></li>
+                <li><a href="deseados.html">Deseados</a></li>
+                <li><a href="tienda.php">Tienda</a></li>
+                <li><a href="publicar.php">Publicar</a></li>
+                <li><a href="cuenta.html">Cuenta</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <div class="divTexto">
+        
+    </div>
+</body>
+</html>
